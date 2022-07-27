27405c41a72ca7173bb3e4ddd48934a941974488
Leonardo Nerone <leofnerone@proton.me>
2022-07-27 13:41:20 -0300

    test(rpc): add `common::main_tests` module

---
+++ tests/common/main_tests/mod.rs
@@ -0,0 +1,11 @@
+mod all_clients_interaction;
+mod basic_daemon_rpc;
+mod basic_wallet;
+mod empty_blockchain;
+mod non_empty_blockchain;
+
+pub use all_clients_interaction::test as all_clients_interaction_test;
+pub use basic_daemon_rpc::test as basic_daemon_rpc_test;
+pub use basic_wallet::test as basic_wallet_test;
+pub use empty_blockchain::test as empty_blockchain_test;
+pub use non_empty_blockchain::test as non_empty_blockchain_test;
