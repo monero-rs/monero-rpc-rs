e7533fe51b18bd2cf65044552100e1074e7cf5c2
Leonardo Nerone <leofnerone@proton.me>
2022-07-27 13:42:57 -0300

    test(rpc): add code to run all tests in `common::main_tests`

--- tests/rpc.rs
+++ tests/rpc.rs
@@ -11,3 +11,25 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+mod common;
+use common::main_tests;
+
+#[tokio::test]
+async fn main_functional_test() {
+    // run those tests functions concurrently since the state one changes does not affect the state
+    // the other one interacts with.
+    let handle1 = tokio::spawn(main_tests::basic_wallet_test());
+    let handle2 = tokio::spawn(async {
+        main_tests::empty_blockchain_test().await;
+        main_tests::non_empty_blockchain_test().await;
+    });
+    let handle3 = tokio::spawn(async {
+        main_tests::basic_daemon_rpc_test().await;
+    });
+
+    let res = tokio::try_join!(handle1, handle2, handle3);
+    res.unwrap();
+
+    main_tests::all_clients_interaction_test().await;
+}
