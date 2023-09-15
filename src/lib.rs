998d79b21338e2e7aa9c4574372743d2219378d4
h4sh3d <h4sh3d@protonmail.com>
2023-09-15 16:25:32 +0200

    chore: update year in headers

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright 2019-2022 Artem Vorotnikov and Monero Rust Contributors
+// Copyright 2019-2023 Artem Vorotnikov and Monero Rust Contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
