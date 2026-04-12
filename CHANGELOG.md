b376c6abaca331d487c931c6c9768a8ad2b27dab
silverpill <87225021+silverpill@users.noreply.github.com>
2026-04-12 10:51:37 +0000

    Define version range for diqwest (#165)
    * Hide unexpected-cfgs warning
    
    * Define version range for diqwest
    
    * Update check-msrv action
    
    * Use MSRV-aware resolver in check-msrv action
    
    * Try MSRV 1.84
    
    * Update changelog
    
    ---------
    
    Co-authored-by: silverpill <silverpill@firemail.cc>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+
+- Defined version range for `diqwest` by @silverpill ([#165](https://github.com/monero-rs/monero-rpc-rs/pull/165)).
+
 ## [0.5.0] - 2025-10-13
 
 ### Added
--- src/models.rs
+++ src/models.rs
@@ -12,10 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// TODO: remove this when clippy error goes away...
-#![allow(clippy::non_canonical_clone_impl)]
-#![allow(unexpected_cfgs)]
-
 use crate::util::*;
 use chrono::prelude::*;
 use monero::{
